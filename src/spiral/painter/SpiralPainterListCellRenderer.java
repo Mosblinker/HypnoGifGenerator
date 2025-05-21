81680041d1c23b99b279bf24f7495ad209d028af
Mosblinker <msteier2.0@gmail.com>
2025-05-20 20:15:12 -0500

    Added list cell renderer for SpiralPainers
    I've added a list cell renderer to render the names of the spirals painted by the spiral painters.

---
+++ src/spiral/painter/SpiralPainterListCellRenderer.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spiral.painter;
+
+import java.awt.Component;
+import javax.swing.*;
+
+/**
+ * This is a list cell renderer that renders the names of 
+ * {@code SpiralPainter}s.
+ * @author Mosblinker
+ * @see SpiralPainter
+ * @see SpiralPainter#getName()
+ */
+public class SpiralPainterListCellRenderer extends DefaultListCellRenderer{
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value, 
+            int index, boolean isSelected, boolean cellHasFocus) {
+            // If the value is a SpiralPainter
+        if (value instanceof SpiralPainter){
+                // Get the name of the spiral
+            String name = ((SpiralPainter)value).getName();
+                // If there is a name for the spiral
+            if (name != null)
+                value = name;
+        }
+        return super.getListCellRendererComponent(list, value, index, 
+                isSelected, cellHasFocus);
+    }
+}
