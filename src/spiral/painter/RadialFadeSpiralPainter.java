c73db6e78a80bff3e40cab79b5edf0e6204a7428
Mosblinker <msteier2.0@gmail.com>
2025-05-24 18:41:20 -0500

    RadialFadeSpiralPainter is now logarithmic
    I've made it so that he RadialFadeSpiralPainter is now logarithmic in nature.

--- src/spiral/painter/RadialFadeSpiralPainter.java
+++ src/spiral/painter/RadialFadeSpiralPainter.java
@@ -5,6 +5,7 @@
 package spiral.painter;
 
 import java.awt.*;
+import java.nio.ByteBuffer;
 
 /**
  *
@@ -13,11 +14,53 @@ import java.awt.*;
 public class RadialFadeSpiralPainter extends SpiralPainter{
     
     public static final Color TRANSPARENT_COLOR = new Color(0x00000000, true);
+    /**
+     * This is the base for the spiral.
+     */
+    private double base = 2.0;
     
     public RadialFadeSpiralPainter() { }
     
     public RadialFadeSpiralPainter(RadialFadeSpiralPainter painter){
         super(painter);
+        this.base = painter.base;
+    }
+    /**
+     * 
+     * @param base 
+     */
+    @Override
+    public void setBase(double base){
+            // If the base is less than 1
+        if (base < 1)
+            throw new IllegalArgumentException();
+            // If the base would change
+        if (this.base != base){
+                // Get the old value for the base
+            double old = this.base;
+            this.base = base;
+            firePropertyChange(BASE_PROPERTY_CHANGED,old,base);
+        }
+    }
+    /**
+     * 
+     * @return 
+     */
+    @Override
+    public double getBase(){
+        return base;
+    }
+    /**
+     * This returns the k value for a logarithmic spiral using the equation 
+     * {@code r=a*e^(k*p)}, with {@code r} being the radius, {@code p} being the 
+     * azimuth, and {@code a} being some constant. This is equal to the tangent 
+     * of the pitch of the spiral.
+     * @return The value multiplied with the azimuth before it is used as the 
+     * exponent for Eular's number.
+     * @see #getBase() 
+     */
+    protected double getLogarithmicK(){
+        return Math.log(getBase());
     }
     @Override
     protected void paintSpiral(Graphics2D g, double angle, int width,int height, 
@@ -28,5 +71,29 @@ public class RadialFadeSpiralPainter extends SpiralPainter{
     @Override
     public String getName() {
         return "Ripple";
+    }
+    @Override
+    protected String paramString(){
+        return super.paramString()+
+                ",base="+getBase();
+    }
+    @Override
+    protected int getByteArrayLength(){
+        return super.getByteArrayLength()+Double.BYTES;
+    }
+    @Override
+    protected void toByteArray(ByteBuffer buffer){
+        super.toByteArray(buffer);
+        buffer.putDouble(getBase());
+    }
+    @Override
+    protected void fromByteArray(ByteBuffer buffer){
+        super.fromByteArray(buffer);
+        setBase(buffer.getDouble());
+    }
+    @Override
+    public void reset(){
+        super.reset();
+        setBase(2.0);
     }
 }
