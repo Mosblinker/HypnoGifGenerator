d032696c66854861efd2192c0d1d549f00e868f1
Mosblinker <msteier2.0@gmail.com>
2025-05-25 11:58:08 -0500

    Reworked Spiral Start code
    I've reworked the code to get the radius of the curve of the spiral. These changes aren't finished.

--- src/spiral/painter/RippleSpiralPainter.java
+++ src/spiral/painter/RippleSpiralPainter.java
@@ -33,23 +33,23 @@ public class RippleSpiralPainter extends LogarithmicSpiralPainter{
             // This gets the amount by which to multiply the angle when 
             // computing the spiral.
         double k = getLogarithmicK();
-            // This is the value by which to adjust the radius to get the other 
-            // spiral that completes the shape
+            // This is the value by which to use to get the actual curve of the 
+            // spiral
         double lim = Math.exp(k * (1-thickness));
-            // This is the value to use to calculate the other spiral that 
-            // completes the path. This is used for the outer spiral curve when 
-            // clockwise and the inner spiral curve when counter-clockwise 
+            // This is the radius of the actual curve of the spiral
         double a = radius;
-            // When the spiral is going clockwise
-        if (clockwise)
-            a /= lim;
-        else
-            a *= lim;
         
             // This gets the starting azimuth for the spiral. This uses the 
             // multiplier for the secondary curve and ignores whether the spiral 
             // is clockwise or not, treating it as if it was always clockwise.
         double p0 = getAzimuth(a,k,getStartRadius(g),angle,true);
+            // If the spiral is going clockwise
+//        if (!clockwise)
+//            a /= lim;
+//        else
+//            a *= lim;
+//            // Get the point in between the inner and outer spirals
+//        a = (radius + a) / 2.0;
         
         System.out.println("Angle: " + angle);
         System.out.println("Angle: " + unadjustRotation(angle,thickness,clockwise));
