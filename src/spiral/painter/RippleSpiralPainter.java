0c14ba9e84ee053b23335226908ddbbe32ec93f0
Mosblinker <msteier2.0@gmail.com>
2025-05-25 10:39:30 -0500

    Copied some of the setup code from parent
    I've copied some of the setup code for the spiral from LogarithmicSpiralPainter. This is the code responsible for calculating the second radius and getting the starting azimuth.

--- src/spiral/painter/RippleSpiralPainter.java
+++ src/spiral/painter/RippleSpiralPainter.java
@@ -29,6 +29,31 @@ public class RippleSpiralPainter extends LogarithmicSpiralPainter{
             double centerX, double centerY, boolean clockwise, double radius, 
             double thickness) {
         super.paintSpiralGegl(g, angle, width, height, centerX, centerY, clockwise, radius, thickness);
+            // This gets the amount by which to multiply the angle when 
+            // computing the spiral.
+        double k = getLogarithmicK();
+            // This is the value by which to adjust the radius to get the other 
+            // spiral that completes the shape
+        double lim = Math.exp(k * (1-thickness));
+            // This is the value to use to calculate the other spiral that 
+            // completes the path. This is used for the outer spiral curve when 
+            // clockwise and the inner spiral curve when counter-clockwise 
+        double a = radius;
+            // When the spiral is going clockwise
+        if (clockwise)
+            a /= lim;
+        else
+            a *= lim;
+        
+            // This gets the starting azimuth for the spiral. This uses the 
+            // multiplier for the secondary curve and ignores whether the spiral 
+            // is clockwise or not, treating it as if it was always clockwise.
+        double p0 = getAzimuth(a,k,getStartRadius(g),angle,true);
+        
+        System.out.println("Angle: " + angle);
+        System.out.println("Angle: " + unadjustRotation(angle,thickness,clockwise));
+        System.out.println("a: " + a);
+        System.out.println("p0: "+ p0);
     }
     @Override
     public String getName() {
