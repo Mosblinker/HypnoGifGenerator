0ca8ababc200f28653a58c41ae17a569a8858553
Mosblinker <msteier2.0@gmail.com>
2025-05-25 16:13:09 -0500

    Added ShapedSpiral interface
    I've added an interface for spirals that have a shape.

---
+++ src/spiral/painter/ShapedSpiral.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package spiral.painter;
+
+/**
+ *
+ * @author Mosblinker
+ */
+public interface ShapedSpiral {
+    /**
+     * 
+     * @return 
+     */
+    public SpiralShape getShape();
+    /**
+     * 
+     * @param shape
+     */
+    public void setShape(SpiralShape shape);
+}
