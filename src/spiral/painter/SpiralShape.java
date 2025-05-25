a75c8f7e84dfbf7c02a995654029866bf486fa4b
Mosblinker <msteier2.0@gmail.com>
2025-05-25 16:08:18 -0500

    Added spiral shape enum and list renderer
    I've added a spiral shape enum and a list renderer to render the names of the spiral shapes.

---
+++ src/spiral/painter/SpiralShape.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package spiral.painter;
+
+/**
+ *
+ * @author Mosblinker
+ */
+public enum SpiralShape {
+    
+    CIRCLE, 
+    
+    SQUARE,
+    
+    DIAMOND
+    
+}
