1c7b9800d419b2320c1c98e0f4cf3ac9ecf37da7
Mosblinker <msteier2.0@gmail.com>
2025-06-04 14:45:57 -0500

    Added default spiral model
    I've added a default mutable version of the spiral model.

---
+++ src/spiral/DefaultSpiralModel.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spiral;
+
+import java.awt.Color;
+import java.util.Objects;
+
+/**
+ *
+ * @author Mosblinker
+ */
+public class DefaultSpiralModel implements SpiralModel{
+    
+    private Color color1;
+    
+    private Color color2;
+    
+    private double rotation;
+    
+    public DefaultSpiralModel(Color color1, Color color2, double angle){
+        this.color1 = Objects.requireNonNull(color1);
+        this.color2 = Objects.requireNonNull(color2);
+        this.rotation = angle;
+    }
+    
+    public DefaultSpiralModel(SpiralModel model){
+        this(model.getColor1(),model.getColor2(),model.getRotation());
+    }
+
+    @Override
+    public Color getColor1() {
+        return color1;
+    }
+    @Override
+    public void setColor1(Color color) {
+        this.color1 = color;
+    }
+    @Override
+    public Color getColor2() {
+        return color2;
+    }
+    @Override
+    public void setColor2(Color color) {
+        this.color2 = color;
+    }
+    @Override
+    public double getRotation() {
+        return rotation;
+    }
+    @Override
+    public void setRotation(double angle) {
+        this.rotation = angle;
+    }
+}
