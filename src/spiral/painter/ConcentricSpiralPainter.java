15d6d79fb0b9419cb6fbcb78ae43aaafbd522c2c
Mosblinker <msteier2.0@gmail.com>
2025-06-05 14:48:55 -0500

    Fixed angle offset when no foreground
    I've fixed the code that offsets the angle when the foreground is transparent

--- src/spiral/painter/ConcentricSpiralPainter.java
+++ src/spiral/painter/ConcentricSpiralPainter.java
@@ -50,7 +50,7 @@ public class ConcentricSpiralPainter extends SpiralPainter implements ShapedSpir
                 // Invert the thickness
             thickness = 1.0-thickness;
                 // Shift it by 180 degrees
-            GeometryMath.boundDegrees(angle+HALF_CIRCLE_DEGREES);
+            angle += HALF_CIRCLE_DEGREES;
                 // Set the color to use to the background color
             g.setColor(model.getColor1());
         } else {
