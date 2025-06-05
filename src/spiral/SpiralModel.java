f45bd0a08195232f5c74ec96082536701ae3bfcd
Mosblinker <msteier2.0@gmail.com>
2025-06-05 15:09:25 -0500

    Made the spiral model interface more abstract
    I've abstracted the spiral model further since some of the stuff has been moved into AbstractSpiralModel.

--- src/spiral/SpiralModel.java
+++ src/spiral/SpiralModel.java
@@ -5,52 +5,84 @@
 package spiral;
 
 import java.awt.Color;
+import java.util.Objects;
 
 /**
  *
  * @author Mosblinker
  */
 public interface SpiralModel {
-    
+    /**
+     * 
+     * @return 
+     */
     public Color getColor1();
-    
+    /**
+     * 
+     * @param color 
+     */
     public void setColor1(Color color);
-    
+    /**
+     * 
+     * @return 
+     */
     public Color getColor2();
-    
+    /**
+     * 
+     * @param color 
+     */
     public void setColor2(Color color);
-    
+    /**
+     * 
+     * @param color1
+     * @param color2 
+     */
     public default void setColors(Color color1, Color color2){
+        Objects.requireNonNull(color1);
+        Objects.requireNonNull(color2);
         setColor1(color1);
         setColor2(color2);
     }
-    
-    public default Color blend(double t){
-        return SpiralGeneratorUtilities.blendColor(getColor1(), getColor2(), t);
-    }
-    
+    /**
+     * 
+     * @param t
+     * @return 
+     */
+    public Color blend(double t);
+    /**
+     * 
+     * @return 
+     */
     public double getRotation();
-    
+    /**
+     * 
+     * @param angle 
+     */
     public void setRotation(double angle);
-    
-    public default double getCenterX(){
-        return 0.5;
-    }
-    
-    public default void setCenterX(double x){
-        throw new UnsupportedOperationException("Position cannot be changed");
-    }
-    
-    public default double getCenterY(){
-        return 0.5;
-    }
-    
-    public default void setCenterY(double y){
-        throw new UnsupportedOperationException("Position cannot be changed");
-    }
-    
-    public default void setCenter(double x, double y){
-        setCenterX(x);
-        setCenterY(y);
-    }
+    /**
+     * 
+     * @return 
+     */
+    public double getCenterX();
+    /**
+     * 
+     * @param x 
+     */
+    public void setCenterX(double x);
+    /**
+     * 
+     * @return 
+     */
+    public double getCenterY();
+    /**
+     * 
+     * @param y 
+     */
+    public void setCenterY(double y);
+    /**
+     * 
+     * @param x
+     * @param y 
+     */
+    public void setCenter(double x, double y);
 }
