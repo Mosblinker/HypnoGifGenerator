d3110d6d1a6021a0c6c005e92014d9e347ee8926
Mosblinker <msteier2.0@gmail.com>
2025-06-05 22:25:15 -0500

    Added more credits
    I've added more credits to the program, along with a place for testers and libraries once I get around to adding those to the credits.

--- src/spiral/SpiralGenerator.java
+++ src/spiral/SpiralGenerator.java
@@ -82,7 +82,16 @@ public class SpiralGenerator extends javax.swing.JFrame implements DebugCapable{
      */
     private static final String[] CREDITS = {
         "                         ---- Developers ----",
-        "Mosblinker - Main developer and artist."
+        "Mosblinker - Main developer and artist.",
+//        "                          ---- Testers ----",
+//        "*Insert Testers Here*",
+//        "                           ---- Libraries ----",
+//        "Thumbnailator - coobird - https://github.com/coobird/thumbnailator",
+//        "SwingExtended - Mosblinker - https://github.com/Mosblinker/SwingExtended",
+//        "FilesExtended - Mosblinker - https://github.com/Mosblinker/FilesExtended",
+//        "GeomArt4J - Mosblinker - https://github.com/Mosblinker/GeomArt4J",
+        "                       ---- Special Thanks ----",
+        "Special thanks to JWolf for the inspiration for this program."
     };
     /**
      * This is the pattern for the file handler to use for the log files of this 
