d394114497eb10d5d68d57d83034643b19ca923b
Mosblinker <msteier2.0@gmail.com>
2025-06-07 02:47:22 -0500

    Moved program icon code into class
    I've moved the icon used to display the program icon on the about window into its own subclass.

--- src/spiral/SpiralGenerator.java
+++ src/spiral/SpiralGenerator.java
@@ -360,44 +360,9 @@ public class SpiralGenerator extends javax.swing.JFrame implements DebugCapable{
         }
         setIconImages(iconImages);
         
-            // Get it to shut up about iconImg not being effectively final
-        BufferedImage iconMask = iconImg;
             // Create the icon for the about window
-        aboutIconLabel.setIcon(new Icon2D(){
-                // The painter for the about icon
-            SpiralPainter painter = iconPainter;
-                // The mask for the about icon
-            BufferedImage mask = iconMask;
-                // The model for the spiral for the about icon
-            SpiralModel model = iconModel;
-                // The model for the spiral overlay for the about icon
-            SpiralModel maskModel = iconMsgModel;
-            @Override
-            public void paintIcon2D(Component c, Graphics2D g, int x, int y) {
-                    // Get the icon's width
-                int width = getIconWidth();
-                    // Get the icon's height
-                int height = getIconHeight();
-                    // Get any scaling transform that's been applied to the 
-                    // graphics context
-                AffineTransform tx = g.getTransform();
-                    // Configure the graphics context
-                g = configureGraphics(g);
-                    // Generate the image for the program icon and draw it to 
-                    // the graphics context
-                g.drawImage(getProgramIcon((int)Math.ceil(width*tx.getScaleX()),
-                        (int) Math.ceil(height * tx.getScaleY()),model,maskModel,
-                        painter,mask), x, y, width, height, c);
-            }
-            @Override
-            public int getIconWidth() {
-                return 128;
-            }
-            @Override
-            public int getIconHeight() {
-                return getIconWidth();
-            }
-        });
+        aboutIconLabel.setIcon(new ProgramIcon(128,iconPainter,iconImg,iconModel,
+                iconMsgModel));
             // Get the document for the credits text pane
         StyledDocument creditsDoc = aboutCreditsTextPane.getStyledDocument();
             // This is a String to get the credits text
@@ -3734,6 +3699,82 @@ public class SpiralGenerator extends javax.swing.JFrame implements DebugCapable{
         @Override
         public int getIconHeight() {
             return getImageHeight();
+        }
+    }
+    /**
+     * 
+     */
+    private class ProgramIcon implements Icon2D{
+        /**
+         * The width and height for this icon.
+         */
+        private int size;
+        /**
+         * The painter for this icon.
+         */
+        private SpiralPainter painter;
+        /**
+         * The mask for the overlay for this icon.
+         */
+        private BufferedImage mask;
+        /**
+         * The model for the main part of the spiral.
+         */
+        private SpiralModel model;
+        /**
+         * The model for the spiral overlay.
+         */
+        private SpiralModel maskModel;
+        /**
+         * This is the image drawn by this icon. This will be null until it's 
+         * used for the first time.
+         */
+        private BufferedImage img = null;
+        /**
+         * 
+         * @param size
+         * @param painter
+         * @param mask
+         * @param model
+         * @param maskModel 
+         */
+        ProgramIcon(int size, SpiralPainter painter, BufferedImage mask, 
+                SpiralModel model, SpiralModel maskModel){
+            this.size = size;
+            this.painter = painter;
+            this.mask = mask;
+            this.model = model;
+            this.maskModel = maskModel;
+        }
+        @Override
+        public void paintIcon2D(Component c, Graphics2D g, int x, int y) {
+                // Get the icon's width
+            int width = getIconWidth();
+                // Get the icon's height
+            int height = getIconHeight();
+                // Get any scaling transform that's been applied to the graphics 
+                // context
+            AffineTransform tx = g.getTransform();
+                // Configure the graphics context
+            g = configureGraphics(g);
+                // The width for the image
+            int w = (int) Math.ceil(width*tx.getScaleX());
+                // The height for the image
+            int h = (int) Math.ceil(height*tx.getScaleY());
+                // If the image is null or the width or height doesn't match
+            if (img == null || img.getWidth() != w || img.getHeight() != h)
+                    // Generate the image for the program icon
+                img = getProgramIcon(w,h,model,maskModel,painter,mask);
+                // Draw the program icon to the graphics context
+            g.drawImage(img, x, y, width, height, c);
+        }
+        @Override
+        public int getIconWidth() {
+            return size;
+        }
+        @Override
+        public int getIconHeight() {
+            return getIconWidth();
         }
     }
     
