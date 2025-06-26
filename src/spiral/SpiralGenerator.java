a96a498693fa746ef2c192916c56e3f00ef3f5d4
Mosblinker <msteier2.0@gmail.com>
2025-06-26 17:35:19 -0500

    Incremented program version
    I've incremented the program version to 0.22.0.

--- src/spiral/SpiralGenerator.java
+++ src/spiral/SpiralGenerator.java
@@ -77,7 +77,7 @@ public class SpiralGenerator extends javax.swing.JFrame implements DebugCapable{
     /**
      * This is the current version of the program.
      */
-    public static final String PROGRAM_VERSION = "0.21.0";
+    public static final String PROGRAM_VERSION = "0.22.0";
     /**
      * This is the name of the program.
      */
