8a574db2cde8d3c36c2908a9ced23722e671f380
Mosblinker <msteier2.0@gmail.com>
2025-08-30 00:06:19 -0500

    Added spiral painter class for oscillating circles
    I've added a spiral painter class for oscillating circles.

---
+++ src/spiral/painter/OscillatingCirclesSpiralPainter.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spiral.painter;
+
+import java.awt.Graphics2D;
+import spiral.SpiralModel;
+
+/**
+ *
+ * @author Mosblinker
+ */
+public class OscillatingCirclesSpiralPainter extends SpiralPainter{
+
+    @Override
+    protected void paintSpiral(Graphics2D g, SpiralModel model, double angle, int width, int height, double centerX, double centerY, boolean clockwise, double radius, double thickness) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public OscillatingCirclesSpiralPainter clone() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
